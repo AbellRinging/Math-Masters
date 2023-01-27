6d6760348884b6449a5df26845c3363ae1c40fd7
TheJokeLion <jlapferreira@gmail.com>
2023-01-27 18:37:10 +0000

    qwe

---
+++ Assets/Scripts/VillageSwitch.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VillageSwitch : MonoBehaviour
+{
+   void onTriggerEnter (Collider other)
+   {
+      SceneManager.LoadScene(2);
+   }
+
+}
