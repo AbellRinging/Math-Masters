863064b6c537dd6f84b5c036c923305fc881c71c
AbelMarques <dayunamite@gmail.com>
2023-02-14 19:48:53 +0000

    Mouse Movement Implemented

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -11,28 +11,8 @@ public class PlayerAnimation : Parent_PlayerScript
     protected override void Custom_Start()
     {
         animator = GetComponent<Animator>();
-        // isWalkingHash = Animator.StringToHash("isWalkingFWD");
     }
 
-    // public void Animate()
-    // {
-    //     bool isWalking = animator.GetBool(isWalkingHash);
-    //     bool forwardPressed = Input.GetKey("w");
-
-    //     //if player presses w Key
-    //     if (!isWalking && forwardPressed)
-    //     {
-    //         // then set the isWalking boolean to true
-    //         animator.SetBool(isWalkingHash, true);
-    //     }
-
-    //     //if player is not pressing w Key
-    //     if (isWalking && !forwardPressed)
-    //     {
-    //         // then set the isWalking boolean to false
-    //         animator.SetBool(isWalkingHash, false);
-    //     }
-    // }
     public void ToggleAnimation(string AnimationName, bool boolean){
         animator.SetBool(AnimationName, boolean);
     }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerMovement : Parent_PlayerScript
 {
-    public float movementSpeed = 5f;
-    private Vector3 Playerforward;
-    private Vector3 right;
-    private Rigidbody rb;
+    #region WASD Movement Variables
+        public float movementSpeed = 5f;
+        private Vector3 Playerforward;
+        private Vector3 right;
+        private Rigidbody rb;
+    #endregion
+
+    #region Mouse Variables
+        private NavMeshAgent NavigationAgent;
+        private RaycastHit ClickedLocation;
+        private bool isMovingToClickedLocation = false;
+        private string groundTag = "Ground";
+    #endregion
 
     protected override void Custom_Start()
     {
@@ -19,28 +29,57 @@ public class PlayerMovement : Parent_PlayerScript
         Playerforward.y = 0;
         Playerforward = Vector3.Normalize(Playerforward);
         right = Quaternion.Euler(new Vector3(0, 90, 0)) * Playerforward;
+
+        // Mouse Movement start
+        NavigationAgent = GetComponent<NavMeshAgent>();
     }
 
     public void Move()
     {
+        // WASD Movement
         if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))) // && !isDashing && !isAttacking)
         {
+            // Stop any mouse movement
+            isMovingToClickedLocation = false;
+            NavigationAgent.velocity = Vector3.zero;
+            NavigationAgent.isStopped = true;
+            NavigationAgent.ResetPath();
+            
             Vector3 rightMovement = right * movementSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
-
             Vector3 upMovement = Playerforward * movementSpeed * Time.deltaTime * Input.GetAxis("Vertical");
-
             Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
             
-            if (heading != Vector3.zero)
-            {
-                transform.forward = heading;
-            }
+            if (heading != Vector3.zero) transform.forward = heading;
 
             rb.velocity = heading * movementSpeed;
+
             MainScript.AnimationScript.ToggleAnimation("isWalkingFWD", true);
         }
-        //Stop movement
-        else
+        // Mouse Click Detection (Only happens if WASD is not being pressed)
+        else if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out ClickedLocation, Mathf.Infinity))
+            {
+                // Clicked on Terrain
+                if (ClickedLocation.collider.CompareTag(groundTag))
+                {
+                    NavigationAgent.SetDestination(ClickedLocation.point);
+                    isMovingToClickedLocation = true;
+
+                    MainScript.AnimationScript.ToggleAnimation("isWalkingFWD", true);
+                }
+                // Clicked on [SOMETHING ELSE] (Call MainScript??)
+            }
+        }
+        // If there is no more player Input, let the character move to the Clicked Location. Checks if the character is still moving
+        else if (isMovingToClickedLocation && !NavigationAgent.hasPath)
+        {
+            isMovingToClickedLocation = false;
+        }
+        // Stop moving
+        else if (!isMovingToClickedLocation)
         {
             rb.velocity = transform.forward * 0;
             MainScript.AnimationScript.ToggleAnimation("isWalkingFWD", false);
